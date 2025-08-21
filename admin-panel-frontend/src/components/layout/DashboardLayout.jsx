a6d11a98e44e19b55a09df41e1001abb0345a15b
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-08-21 16:55:08 +0530

    refactor: Update DashboardLayout to improve page title formatting and clean up code

--- admin-panel-frontend/src/components/layout/DashboardLayout.jsx
+++ admin-panel-frontend/src/components/layout/DashboardLayout.jsx
@@ -3,8 +3,11 @@ import { Sidebar } from "./Sidebar/Sidebar";
 
 const DashboardLayout = ({ children, onNavigate, currentPage, userRole }) => {
     const { userId } = useAuth();
+    console.log(userId)
 
     const formatPageTitle = (activePageTitle) => {
+        if (currentPage == "profile-management") return "Profile"
+        if (currentPage == "user-dashboard") return "Dashboard"
         return activePageTitle.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
     };
 
@@ -13,15 +16,16 @@ const DashboardLayout = ({ children, onNavigate, currentPage, userRole }) => {
         <div className="min-h-screen flex flex-col md:flex-row bg-gray-100">
             <Sidebar onNavigate={onNavigate} currentPage={currentPage} userRole={userRole} />
             <div className="flex-grow bg-white rounded-lg shadow-xl p-6 md:mt-0 overflow-auto">
-                <header className="mb-8 pb-4 border-b border-gray-200">
-                    <p className="text-3xl font-extrabold text-gray-900 mb-2">
+                <header className="mb-8 border-b border-gray-200">
+                    <p className="text-3xl font-extrabold text-gray-900 pb-4 ">
                         {formatPageTitle(currentPage)}
                     </p>
-                    {userId && (
+                    {/* {userId && (
                         <p className="text-sm text-gray-500">
                             Logged in as: <span className="font-mono bg-gray-100 px-2 py-1 rounded-md text-xs">{userId}</span>
                         </p>
-                    )}
+                    )} */}
+
                 </header>
                 <main className="min-h-[70vh]">
                     {children}
