5152d994df6b46c60f6aa5c004a7d5c267827bc9
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-09-01 10:32:45 +0530

    feat: add new api service for admin in which implemeted the admin api handling

---
+++ admin-panel-frontend/src/services/AdminService.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const AdminService = {
+    registerUser: async (userData) => {
+        try {
+            const response = await axios.post(`${API_BASE_URL}/admin/create-user`, userData);
+            return response.data;
+        } catch (error) {
+            throw error.response?.data || { message: "Failed to register user" };
+        }
+    },
+    getAllUsers: async () => {
+        try {
+            const response = await axios.get(`${API_BASE_URL}/admin/users`);
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching users:", error);
+            throw error.response?.data || { message: "Failed to fetch users" };
+        }
+    },
+    deleteUser: async (userId) => {
+        try {
+            const response = await axios.delete(`${API_BASE_URL}/admin/user/delete/${userId}`);
+            return response.data;
+        } catch (error) {
+            throw error.response?.data || { message: "Failed to delete user" };
+        }
+    },
+    getUserById: async (userId) => {
+        try {
+            const response = await axios.get(`${API_BASE_URL}/admin/users/${userId}`);
+            return response.data;
+        } catch (error) {
+            throw error.response?.data || { message: "Failed to fetch user" };
+        }
+    },
+    activityLog: async (userId) => {
+        try {
+            const response = await axios.get(`${API_BASE_URL}/admin/activities/user/${userId}`);
+            return response.data;
+        } catch (error) {
+            throw error.response?.data || { message: "Failed to fetch activity log" };
+        }
+    }
+}
