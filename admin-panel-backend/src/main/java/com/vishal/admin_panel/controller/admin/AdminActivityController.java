9af76f260d6f4ded5f0b8301e40fc089bd69e22a
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-09-01 10:30:02 +0530

    commented code and moved in another activityController file

--- admin-panel-backend/src/main/java/com/vishal/admin_panel/controller/admin/AdminActivityController.java
+++ admin-panel-backend/src/main/java/com/vishal/admin_panel/controller/admin/AdminActivityController.java
@@ -1,29 +1,22 @@
 package com.vishal.admin_panel.controller.admin;
 
-import com.vishal.admin_panel.dto.UserActivityDTO;
 import com.vishal.admin_panel.entity.UserActivity;
 import com.vishal.admin_panel.service.UserActivityService;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin")
-
+@RequestMapping("/api/admin/activities")
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminActivityController {
 
     private final UserActivityService userActivityService;
 
     public AdminActivityController(UserActivityService userActivityService) {
         this.userActivityService = userActivityService;
-    }
-
-    // GET all activities
-    @GetMapping("/activities")
-    public ResponseEntity<List<UserActivityDTO>> getActivities() {
-        return ResponseEntity.ok(userActivityService.getAllActivities());
     }
 
     // GET activities by user
--- admin-panel-backend/src/main/java/com/vishal/admin_panel/controller/admin/AdminController.java
+++ admin-panel-backend/src/main/java/com/vishal/admin_panel/controller/admin/AdminController.java
@@ -2,7 +2,7 @@ package com.vishal.admin_panel.controller.admin;
 
 import java.util.List;
 
- import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
+
     private final UserService userService;
 
     @GetMapping("/test")
@@ -41,10 +42,10 @@ public class AdminController {
         return userService.getAllUsers();
     }
 
-//    @PutMapping("/user/update/{id}")
-//    public User updateUser(@PathVariable Long id, @RequestBody User user) {
-//        return userService.updateUser(id, user);
-//    }
+    // @PutMapping("/user/update/{id}")
+    // public User updateUser(@PathVariable Long id, @RequestBody User user) {
+    // return userService.updateUser(id, user);
+    // }
 
     @DeleteMapping("/user/delete/{id}")
     public void deleteUser(@PathVariable Long id) {
