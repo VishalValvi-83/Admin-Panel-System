ad1b134b11846d31d03a45a2ed82932b2641fa7c
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-09-11 12:41:15 +0530

    page navingation corrected

--- admin-panel-frontend/src/App.jsx
+++ admin-panel-frontend/src/App.jsx
@@ -1,35 +1,36 @@
 import React, { useContext, useEffect, useState } from 'react'
-import AdminDashboard from './pages/Dashboards/Admin/AdminDashboard'
-import TaskManager from './pages/Dashboards/Admin/TaskManager'
-import AnalyticsDashboard from './pages/Dashboards/Admin/AnalyticsDashboard'
-import SystemConfiguration from './pages/Dashboards/Admin/SystemConfiguration'
-import { Login } from './pages/Auth/Login'
-import { LoadingSpinner } from './components/LoadingSpinner/LoadingSpinner'
-import DashboardLayout from './components/layout/DashboardLayout'
-import UserDashboard from './pages/Dashboards/User/UserDashboard'
-import ProfileManagement from './pages/Dashboards/profile/ProfileManagement'
-import ManagerDashboard from './pages/Dashboards/Manager/ManagerDashboard'
-import AssignedUsers from './pages/Dashboards/Manager/AssignedUsers'
-import DepartmentAnalytics from './pages/Dashboards/Manager/DepartmentAnalytics'
-import { AuthContext } from './context/AuthContext2'
+import AdminDashboard from './pages/Dashboards/Admin/AdminDashboard';
+import UserManagement from './pages/Dashboards/Admin/UserManagement';
+import TaskManager from './pages/Dashboards/Admin/TaskManager';
+import AnalyticsDashboard from './pages/Dashboards/Admin/AnalyticsDashboard';
+import SystemConfiguration from './pages/Dashboards/Admin/SystemConfiguration';
+import { Login } from './pages/Auth/Login';
+import { LoadingSpinner } from './components/LoadingSpinner/LoadingSpinner';
+import DashboardLayout from './components/layout/DashboardLayout';
+import UserDashboard from './pages/Dashboards/User/UserDashboard';
+import ProfileManagement from './pages/Dashboards/profile/ProfileManagement';
+import ManagerDashboard from './pages/Dashboards/Manager/ManagerDashboard';
+import AssignedUsers from './pages/Dashboards/Manager/AssignedUsers';
+import DepartmentAnalytics from './pages/Dashboards/Manager/DepartmentAnalytics';
+import { AuthContext } from './context/AuthContext2';
 import { Toaster } from 'react-hot-toast';
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom';
+
 const App = () => {
-  const { isAuthenticated, userRole, userId, loading, logout } = useContext(AuthContext);  // const [isNewAuthenticated, setIsNewAuthenticated] = useState(false);
-  const [currentPage, setCurrentPage] = useState('user-management');
+  const { isAuthenticated, userRole, userId, loading, logout } = useContext(AuthContext);
+  const [currentPage, setCurrentPage] = useState('admin-dashboard');
 
   const [authLoading, setAuthLoading] = useState(loading);
 
   useEffect(() => {
     if (isAuthenticated) {
-      setCurrentPage(userRole === 'ADMIN' ? 'user-management' : userRole === 'MANAGER' ? 'manager-dashboard' : 'user-dashboard');
+      setCurrentPage(userRole === 'ADMIN' ? 'admin-dashboard' : userRole === 'MANAGER' ? 'manager-dashboard' : 'user-dashboard');
     } else {
       setTimeout(() => {
         setAuthLoading(false);
       }, 2000);
       setCurrentPage('');
     }
-
   }, [userId, isAuthenticated, userRole]);
 
   if (authLoading) {
@@ -39,8 +40,10 @@ const App = () => {
   const renderPageContent = () => {
     switch (currentPage) {
       // Admin Pages
-      case 'user-management':
+      case 'admin-dashboard':
         return <AdminDashboard />;
+      case 'user-management':
+        return <UserManagement />;
       case 'task-manager':
         return <TaskManager />;
       case 'analytics':
